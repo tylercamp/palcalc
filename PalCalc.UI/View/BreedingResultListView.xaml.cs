86bd96ea1b02586248f80c2e59fbfb7487b66de6
Tyler Camp <tylermatthewcamp@gmail.com>
2024-05-11 14:45:47 -0400

    Sort by breeding estimate by default

--- PalCalc.UI/View/BreedingResultListView.xaml.cs
+++ PalCalc.UI/View/BreedingResultListView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PalCalc.UI.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace PalCalc.UI.View
         public BreedingResultListView()
         {
             InitializeComponent();
+
+            Wpf.Util.GridViewSort.ApplySort(Items, nameof(BreedingResultViewModel.TimeEstimate));
         }
     }
 }
