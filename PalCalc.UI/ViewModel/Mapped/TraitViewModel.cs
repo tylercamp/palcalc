8bdd348df5253cd200ca3b144ad365a5590d49e5
Tyler Camp <tylermatthewcamp@gmail.com>
2024-05-15 12:31:48 -0400

    Fix XAML binding errors when displaying a Trait

--- PalCalc.UI/ViewModel/Mapped/TraitViewModel.cs
+++ PalCalc.UI/ViewModel/Mapped/TraitViewModel.cs
@@ -2,25 +2,39 @@
 using PalCalc.UI.Model;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using System.Windows.Media;
 
 namespace PalCalc.UI.ViewModel.Mapped
 {
+    public class RankColorConverter : IValueConverter
+    {
+        private Dictionary<Color, SolidColorBrush> brushes = new Dictionary<Color, SolidColorBrush>();
+
+        private SolidColorBrush MakeBrush(Color color) => brushes.ContainsKey(color) ? brushes[color] : brushes[color] = new SolidColorBrush(color) { Opacity = 1 };
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return MakeBrush((int)value switch
+            {
+                < 0 => new Color() { R = 247, G = 63, B = 63, A = 255 },
+                > 1 => new Color() { R = 255, G = 221, B = 0, A = 255 },
+                _ => new Color() { R = 230, G = 231, B = 223, A = 255 },
+            });
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public class TraitViewModel
     {
-        public static Dictionary<int, Color> RankColors = new Dictionary<int, Color>()
-        {
-            { -3, new Color() { R = 247, G = 63, B = 63, A = 255 } },
-            { -2, new Color() { R = 247, G = 63, B = 63, A = 255 } },
-            { -1, new Color() { R = 247, G = 63, B = 63, A = 255 } },
-            { 0, new Color() { R = 230, G = 231, B = 223, A = 255 } },
-            { 1, new Color() { R = 230, G = 231, B = 223, A = 255 } },
-            { 2, new Color() { R = 255, G = 221, B = 0, A = 255 } },
-            { 3, new Color() { R = 255, G = 221, B = 0, A = 255 } },
-        };
 
         // for XAML designer view
         public TraitViewModel()
@@ -36,8 +50,9 @@ namespace PalCalc.UI.ViewModel.Mapped
         public Trait ModelObject { get; }
 
         public ImageSource RankIcon => TraitIcon.Images[ModelObject.Rank];
-        public Color RankColor => RankColors[ModelObject.Rank];
 
+        public int Rank => ModelObject.Rank;
+        
         public string Name => ModelObject?.Name ?? "None";
 
         public override bool Equals(object obj) => ModelObject.Equals((obj as TraitViewModel)?.ModelObject);
