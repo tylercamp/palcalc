f3b4bd1f7a5f2d1a68665e322fb25bce999e578e
Tyler Camp <tylermatthewcamp@gmail.com>
2024-11-02 11:34:15 -0400

    Bump version for proper release

--- PalCalc.UI/App.xaml.cs
+++ PalCalc.UI/App.xaml.cs
@@ -18,7 +18,7 @@ namespace PalCalc.UI
     /// </summary>
     public partial class App : Application
     {
-        public static string Version => "v1.7.6-BETA-1";
+        public static string Version => "v1.7.6";
         public static string RepositoryUrl => "https://github.com/tylercamp/palcalc/";
 
         private static ILogger logger;
