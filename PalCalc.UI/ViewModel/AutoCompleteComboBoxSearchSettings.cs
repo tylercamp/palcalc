f283712b2e56afca225bd45e00a2879b55f24f0c
Tyler Camp <tylermatthewcamp@gmail.com>
2025-02-17 11:45:16 -0500

    Use fuzzy string matching for autocomplete dropdowns

---
+++ PalCalc.UI/ViewModel/AutoCompleteComboBoxSearchSettings.cs
@@ -0,0 +1,29 @@
+﻿using DotNetKit.Windows.Controls;
+using FuzzySharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalCalc.UI.ViewModel
+{
+    internal class AutoCompleteComboBoxSearchSettings : AutoCompleteComboBoxSetting
+    {
+        private AutoCompleteComboBoxSearchSettings() { }
+
+        public static AutoCompleteComboBoxSearchSettings Instance { get; } = new();
+
+        public override Predicate<object> GetFilter(string query, Func<object, string> stringFromItem)
+        {
+            return obj =>
+            {
+                var s = stringFromItem(obj);
+                var weight = Fuzz.WeightedRatio(query, stringFromItem(obj));
+
+                return stringFromItem(obj).Contains(query, StringComparison.InvariantCultureIgnoreCase) || weight > 60;
+            };
+        }
+    }
+}
