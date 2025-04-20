c77598a6159dec27fe1d3ae069dd95753c2ab47b
Tyler Camp <tylermatthewcamp@gmail.com>
2025-04-20 11:03:47 -0400

    Add TranslationDebugWindow tab showing total reference counts

---
+++ PalCalc.UI/Localization/TranslatorStatistics.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalCalc.UI.Localization
+{
+    public static partial class Translator
+    {
+        private static Dictionary<LocalizationCodes, int> itlCodeUsage = null;
+        internal static Dictionary<LocalizationCodes, int> ItlCodeUsage => itlCodeUsage ??= Enum.GetValues<LocalizationCodes>().ToDictionary(c => c, c => 0);
+
+        public static T WithCodeUsage<T>(Func<Dictionary<LocalizationCodes, int>, T> fn)
+        {
+            Dictionary<LocalizationCodes, int> counts;
+            lock (ItlCodeUsage)
+                counts = ItlCodeUsage;
+
+            return fn(counts);
+        }
+    }
+}
