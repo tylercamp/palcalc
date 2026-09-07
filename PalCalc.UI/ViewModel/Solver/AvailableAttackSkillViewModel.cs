e15fbf3b90b288452ac0ca1271c9725c80fb199b
Tyler Camp <tylermatthewcamp@gmail.com>
2026-09-07 10:48:43 -0400

    Show attack skill availability warnings when menu is closed

--- PalCalc.UI/ViewModel/Solver/AvailableAttackSkillViewModel.cs
+++ PalCalc.UI/ViewModel/Solver/AvailableAttackSkillViewModel.cs
@@ -1,9 +1,6 @@
 ﻿using PalCalc.UI.ViewModel.Mapped;
+using PalCalc.UI.Localization;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PalCalc.UI.ViewModel.Solver
 {
@@ -18,6 +15,13 @@ namespace PalCalc.UI.ViewModel.Solver
     {
         public ActiveSkillViewModel Attack => vm;
         public AttackSkillAvailability Availability => availability;
+        public ILocalizedText WarningText { get; } = availability switch
+        {
+            AttackSkillAvailability.Available => null,
+            AttackSkillAvailability.NotInheritable => LocalizationCodes.LC_ATTACK_SKILL_NOT_INHERITABLE.Bind(),
+            AttackSkillAvailability.NotKnownByPals => LocalizationCodes.LC_ATTACK_SKILL_NOT_KNOWN.Bind(),
+            _ => throw new ArgumentOutOfRangeException(nameof(availability), availability, null)
+        };
 
         public bool IsAvailable => Availability == AttackSkillAvailability.Available;
     }
